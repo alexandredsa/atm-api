38017fc509992d58ec03bf9acaabb60f595b3425
Alexandre Alves <alexandredias1995@gmail.com>
2020-11-09 11:52:29 -0300

    chore: add comments to doc

--- repositories/withdrawal/default_repository.go
+++ repositories/withdrawal/default_repository.go
@@ -1,12 +1,15 @@
 package withdrawal
 
+//DefaultRepository sample basic repository for withdrawals
 type DefaultRepository struct {
 }
 
+//GetAvailableBanknotesValues returns available notes on ATM
 func (d DefaultRepository) GetAvailableBanknotesValues() []int {
 	return []int{50, 10, 5, 1}
 }
 
+//CreateDefaultRepository returns a new instance of DefaultRepository
 func CreateDefaultRepository() BaseRepository {
 	return DefaultRepository{}
 }
